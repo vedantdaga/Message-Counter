4344977f589ad5cab1d3e18ddd788ccc74f3fcbf
Vedant Daga <vedant.a.daga@gmail.com>
2020-05-25 23:16:31 -0700

    Redid logic to output directly to CSV

--- messagecounter.py
+++ messagecounter.py
@@ -4,28 +4,24 @@ import csv
 
 
 def counter():
-    datelist = []  # initialize list to store date and message count pairs
     currdate = datetime.datetime(2015, 10, 8)  # set current date to the start date of message counting
-
-    todaystring = date.today().strftime("%b %d, %Y")  # convert today's date to a string. used as end date
     datestring = currdate.strftime("%b %d, %Y")  # put date into MMM dd, YYYY format
+    todaystring = date.today().strftime("%b %d, %Y")  # convert today's date to a string. used as end date
+
     f = open("messages.html").read()  # read the messages file
 
-    while datestring != todaystring:
-        if datestring[4] == "0":
-            datestring = datestring[:4] + datestring[5:]
+    with open('messagecsv.csv', 'wb') as outputfile:
+        wr = csv.writer(outputfile)
+        while datestring != todaystring:
+            if datestring[4] == "0":
+                datestring = datestring[:4] + datestring[5:]
 
-        number = f.count(datestring)  # count the number of times the date appears in the file
-        print datestring, " - ", number, " messages"  # debugging check
-        datelist.append((datestring, number))  # add to list
+            number = f.count(datestring)  # count the number of times the date appears in the file
+            print datestring, " - ", number, " messages"  # debugging check
+            wr.writerow([datestring, number])
 
-        currdate += datetime.timedelta(days=1)  # next date
-        datestring = currdate.strftime("%b %d, %Y")  # put date into MMM dd, YYYY format
-
-    with open('messagecsv.csv', 'wb') as f:
-        wr = csv.writer(f)
-        for each in datelist:  # convert the list to a .csv file
-            wr.writerow([each[0], each[1]])
+            currdate += datetime.timedelta(days=1)  # next date
+            datestring = currdate.strftime("%b %d, %Y")  # put date into MMM dd, YYYY format
 
 
 if __name__ == '__main__':
